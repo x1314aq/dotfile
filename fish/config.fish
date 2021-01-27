bedd621e0bf3c7d387e2705449fc7fc71a46d312
x1314aq <469221983@qq.com>
2021-01-27 11:02:31 +0800

    update  fish/config.fish.

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,21 @@
-set -xU EDITOR nvim
-set -xU VISUAL nvim
+set -U fish_greeting
+set -U fish_user_paths $fish_user_paths
+
+set -xU EDITOR vim
+set -xU VISUAL vim
+
+set -xU LESS_TERMCAP_md (printf "\e[01;31m")
+set -xU LESS_TERMCAP_me (printf "\e[0m")
+set -xU LESS_TERMCAP_se (printf "\e[0m")
+set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")
+set -xU LESS_TERMCAP_ue (printf "\e[0m")
+set -xU LESS_TERMCAP_us (printf "\e[01;32m")
+
+set -xU no_proxy "localhost,127.0.0.1,10.0.2.*"
+
+set -xU LANG en_US.UTF-8
+set -xU LC_ALL en_US.UTF-8
+
+if test -f ~/.config/fish/fish_alias
+        source ~/.config/fish/fish_alias
+end
