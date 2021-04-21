941db6c8dbbc5b92a3ede07e5a8023c9ce6e20ad
x1314aq <x1314aq@sina.cn>
2021-04-21 23:35:08 +0800

    add fzy.lua

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -19,3 +19,8 @@ utils.map('i', '<Del>', '')
 
 utils.map('n', '<leader>cw', ':cwindow<CR>')
 utils.map('n', '<leader>cc', ':cclose<CR>')
+
+fzy = require('fzy')
+
+utils.map('n', '<leader>f', ':lua fzy.execute("fd", fzy.sinks.edit_file)<CR>', {silent = true})
+utils.map('n', '<leader>b', ':lua fzy.actions.buffers()<CR>', {silent = true})
