0f8142ed709cbd65d214e440110b1d98ca804571
x1314aq <469221983@qq.com>
2021-06-26 03:40:22 +0000

    popopopo

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -31,17 +31,43 @@ end
 
 local term_buf = -1
 
-function M.toggle_terminal()
+local function popup_create()
+  local columns = vim.api.nvim_get_option('columns')
+  local lines = vim.api.nvim_get_option('lines')
+  local opts = {
+    relative = 'editor',
+    style = 'minimal',
+    height = math.floor(lines * 0.8),
+    width = math.floor(columns * 0.8),
+    row = math.floor(lines * 0.1),
+    col = math.floor(columns * 0.1),
+    border = 'single',
+    noautocmd = true,
+  }
+  local win = vim.api.nvim_open_win(term_buf, true, opts)
+  vim.api.nvim_win_set_option(win,  'winhl', 'Normal:Normal')
+end
+
+function M.toggle_terminal(exited)
   local shell = vim.env.SHELL or 'bash'
-  if vim.api.nvim_get_buf_options(0, 'buftype') == 'terminal' then
-    vim.api.nvim_win_close(0)
+  if vim.api.nvim_buf_get_option(0, 'buftype') == 'terminal' then
+    vim.api.nvim_win_close(0, true)
+    if exited == true then
+      vim.api.nvim_buf_delete(term_buf, {force = true})
+      term_buf = -1
+    end
   else
     if term_buf ~= -1 then
-      vim.cmd('vertical sb ' .. tostring(term_buf))
+      popup_create()
     else
-      vim.cmd('vs term://' .. shell)
+      popup_create()
+      vim.cmd('e term://' .. shell)
       term_buf = vim.api.nvim_win_get_buf(0)
       vim.api.nvim_buf_set_keymap(term_buf, 't', '<Esc>', '<c-\\><c-n>', {silent = true, nowait = true})
+      vim.api.nvim_buf_set_option(term_buf, 'buflisted', false)
+      vim.cmd([[
+        au TermClose <buffer> lua require('utils').toggle_terminal(true)
+      ]])
     end
     vim.cmd('startinsert!')
   end
