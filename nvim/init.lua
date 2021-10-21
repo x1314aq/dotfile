0d62637fa3cc50ede10625fb28e8fd0496b27785
x1314aq <x1314aq@sina.cn>
2021-10-22 00:05:28 +0800

    use nvim-cmp instead of nvim-compe

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,9 +4,7 @@ local cmd = vim.cmd
 local Plug = require('pmanager').plug
 
 Plug {'nvim-lua/plenary.nvim', dest='start'}
-Plug {'nvim-lua/popup.nvim', dest='start'}
 Plug {'neovim/nvim-lspconfig', dest='start'}
-Plug {'hrsh7th/nvim-compe', dest='start'}
 Plug {'nvim-treesitter/nvim-treesitter', dest='start'}
 Plug {'nvim-treesitter/nvim-treesitter-textobjects', dest='start'}
 Plug {'nvim-telescope/telescope.nvim', dest='start'}
@@ -14,6 +12,11 @@ Plug {'navarasu/onedark.nvim', dest='start'}
 Plug {'hoob3rt/lualine.nvim', dest='start'}
 Plug {'phaazon/hop.nvim', dest='start'}  -- easy motion plugin
 Plug {'nvim-telescope/telescope-fzy-native.nvim', dest='start'}
+Plug {'hrsh7th/cmp-nvim-lsp', dest='start', branch='main'}
+Plug {'hrsh7th/cmp-buffer', dest='start', branch='main'}
+Plug {'hrsh7th/nvim-cmp', dest='start', branch='main'}
+Plug {'L3MON4D3/LuaSnip', dest='start'}
+Plug {'saadparwaiz1/cmp_luasnip', dest='start'}
 
 -- Sensible defaults
 require('settings')
--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -7,7 +7,7 @@ local function list_or_jump(action, opts)
   local params = vim.lsp.util.make_position_params()
   local result, err = vim.lsp.buf_request_sync(0, action, vim.tbl_extend('error', params, opts), 1000)
   if err then
-    vim.api.nvim_err_writeln("Error when executing " .. action .. " : " .. err)
+    vim.api.nvim_err_writeln('Error when executing ' .. action .. ' : ' .. err)
     return
   end
 
@@ -33,6 +33,9 @@ end
 M.find_callee = function()
   list_or_jump('$ccls/call', {callee = true})
 end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 local nvim_lsp = require('lspconfig')
 local on_attach = function(client, bufnr)
@@ -63,13 +66,15 @@ if vim.fn.executable("ccls") == 1 then
     on_attach = on_attach,
     init_options = {
       cache = {directory = ".ccls-cache"}
-    }
+    },
+    capabilities = capabilities,
   }
 elseif vim.fn.executable("clangd") == 1 then
   print("LSP server is clangd")
   nvim_lsp.clangd.setup {
     autostart = false,
     on_attach = on_attach,
+    capabilities = capabilities,
   }
 end
 
@@ -83,7 +88,7 @@ if sumneko_binary and sumneko_root_path then
   table.insert(runtime_path, "lua/?.lua")
   table.insert(runtime_path, "lua/?/init.lua")
 
-  require'lspconfig'.sumneko_lua.setup {
+  require('lspconfig').sumneko_lua.setup {
     autostart = true,
     on_attach = on_attach,
     cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
@@ -115,67 +120,54 @@ else
   print("ENVIRONMENT variable 'sumneko_binary' or 'sumneko_root_path' not set!")
 end
 
--- nvim-compe config
-require('compe').setup {
-  enabled = true,
-  autocomplete = true,
-  debug = false,
-  min_length = 2,
-  preselect = 'disable',
-  throttle_time = 80,
-  source_timeout = 200,
-  incomplete_delay = 400,
-  max_abbr_width = 100,
-  max_kind_width = 100,
-  max_menu_width = 100,
-  documentation = false,
-  source = {
-    path = true,
-    nvim_lsp = true,
-  };
+-- nvim-cmp config
+local luasnip = require('luasnip')
+local cmp = require('cmp')
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-y>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+  },
+  completion = {
+    keyword_length = 2,
+  },
 }
-
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-  local col = vim.fn.col('.') - 1
-  if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-    return true
-  else
-    return false
-  end
-end
-
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-n>"
-  elseif check_back_space() then
-    return t "<Tab>"
-  else
-    return vim.fn['compe#complete']()
-  end
-end
-
-_G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-p>"
-  else
-    return t "<S-Tab>"
-  end
-end
-
-vim.api.nvim_set_keymap('i', "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap('s', "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap('i', "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap('s', "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-
-vim.api.nvim_set_keymap('i', "<CR>", "compe#confirm('<CR>')", {expr = true, silent = true, noremap = true})
-vim.api.nvim_set_keymap('i', "<c-e>", "compe#close('<c-e>')", {expr = true, silent = true, noremap = true})
 
 -- treesitter config
 require('nvim-treesitter.configs').setup {
