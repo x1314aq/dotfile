bf9fd321cac57762cc9b9953a46193888cf06fc4
x1314aq <469221983@qq.com>
2021-11-08 01:20:21 +0000

    update plugin configs

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -172,16 +172,6 @@ require('nvim-autopairs').setup({
   enable_check_bracket_line = false,
 })
 
-require("nvim-autopairs.completion.cmp").setup({
-  map_cr = false, --  map <CR> on insert mode
-  map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-  auto_select = false, -- automatically select the first item
-  insert = false, -- use insert confirm behavior instead of replace
-  map_char = { -- modifies the function or method delimiter by filetypes
-    all = '(',
-  }
-})
-
 -- treesitter config
 require('nvim-treesitter.configs').setup {
   ensure_installed = {"c", "cpp", "lua"},
