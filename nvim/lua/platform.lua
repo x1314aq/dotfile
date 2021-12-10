3ead36d32cf45715f5f6e00bb369e5a46396e141
x1314aq <469221983@qq.com>
2021-12-10 09:30:37 +0000

    update platform.lua

--- nvim/lua/platform.lua
+++ nvim/lua/platform.lua
@@ -1,6 +1,6 @@
 if jit.os == 'Windows' then
     seperator = '\\'
-    vim.g.python3_host_prog = 'py.exe'
+    vim.g.python3_host_prog = 'python.exe'
 else
     seperator = '/'
     vim.g.python3_host_prog = 'python3'
