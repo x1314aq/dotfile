dfb97edffe520293050082ab6653b7d4f9f83ead
x1314aq <x1314aq@sina.cn>
2022-01-29 22:01:27 +0800

    remove tree-sitter text objects

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -177,41 +177,41 @@ require('nvim-treesitter.configs').setup {
   indent = {
     enable = true
   },
-  textobjects = {
-    select = {
-      enable = true,
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
+  --textobjects = {
+  --  select = {
+  --    enable = true,
+  --    keymaps = {
+  --      -- You can use the capture groups defined in textobjects.scm
+  --      ["af"] = "@function.outer",
+  --      ["if"] = "@function.inner",
+  --      ["ac"] = "@class.outer",
+  --      ["ic"] = "@class.inner",
 
-        -- Or you can define your own textobjects like this
-        ["iF"] = {
-          lua = "(function_definition) @function",
-          cpp = "(function_definition) @function",
-          c = "(function_definition) @function",
-        },
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        ["]f"] = "@function.outer",
-      },
-      goto_next_end = {
-        ["]F"] = "@function.outer",
-      },
-      goto_previous_start = {
-        ["[f"] = "@function.outer",
-      },
-      goto_previous_end = {
-        ["[F"] = "@function.outer",
-      },
-    },
-  },
+  --      -- Or you can define your own textobjects like this
+  --      ["iF"] = {
+  --        lua = "(function_definition) @function",
+  --        cpp = "(function_definition) @function",
+  --        c = "(function_definition) @function",
+  --      },
+  --    },
+  --  },
+  --  move = {
+  --    enable = true,
+  --    set_jumps = true, -- whether to set jumps in the jumplist
+  --    goto_next_start = {
+  --      ["]f"] = "@function.outer",
+  --    },
+  --    goto_next_end = {
+  --      ["]F"] = "@function.outer",
+  --    },
+  --    goto_previous_start = {
+  --      ["[f"] = "@function.outer",
+  --    },
+  --    goto_previous_end = {
+  --      ["[F"] = "@function.outer",
+  --    },
+  --  },
+  --},
 }
 
 return M
