1e01df4eaa61e56bc9cee0727798a6ea0687d600
x1314aq <x1314aq@sina.cn>
2023-10-14 15:29:55 +0800

    [nvim]: update indent-blankline.nvim to version 3

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,9 +1,10 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = { "BufReadPost", "BufNewFile" },
+  main = "ibl",
   opts = {
-    char = '|',
-    buftype_exclude = {'terminal'},
-    space_char_blankline = ' ',
+    indent = {
+      char = '|',
+    },
   },
 }
